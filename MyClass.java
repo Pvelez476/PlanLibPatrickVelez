76435b2c9c7c426d596932a5ba7ca9f8fb650ceb
Pvelez476 <pvelez@students.mchenry.edu>
2015-11-17 02:30:38 -0600

    Initial commit

---
+++ MyClass.java
@@ -0,0 +1,69 @@
+package com.example;
+
+import java.util.*;
+
+
+public class MyClass {
+
+    //MAIN METHOD.
+    public static void main(String[] args) {
+
+
+        //Code to call Calendar.
+        Calendar cal = Calendar.getInstance();
+
+
+        System.out.println("Calendar's Year: " + cal.get(Calendar.YEAR));
+        System.out.println("Calendar's Month: " + cal.get(Calendar.MONTH));
+        System.out.println("Calendar's Day: " + cal.get(Calendar.DATE));
+
+        //Code to call Time.
+        System.out.println("The Hour is: " + cal.get(Calendar.HOUR_OF_DAY));
+        System.out.println("The Minute is: " + cal.get(Calendar.MINUTE));
+        System.out.println("The Second is: " + cal.get(Calendar.SECOND));
+        //////////////////////////////////////////////////////////////////
+
+
+
+        //Ints to represent the days.
+        int sunday = 1;
+        int monday = 2;
+        int tuesday = 3;
+        int wednesday = 4;
+        int thursday = 5;
+        int friday = 6;
+        int saturday = 7;
+
+        //If Statements to call days.
+        if (sunday >= 1) {
+            System.out.println("Sunday");
+        }
+
+        if (monday >= 2) {
+            System.out.println("Monday");
+        }
+
+        if (tuesday >= 3) {
+            System.out.println("Tuesday");
+        }
+
+        if (wednesday >= 4) {
+            System.out.println("Wednesday");
+        }
+
+        if (thursday >= 5) {
+            System.out.println("Thursday");
+        }
+
+        if (friday >= 6) {
+            System.out.println("Friday");
+        }
+
+        if (saturday >= 7) {
+            System.out.println("Saturday");
+        }
+
+
+    }
+
+}
